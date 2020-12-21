3ba59202676d704ac2532138c30c9239ed9ac71d
Chocla <joe.mcdonough@live.com>
2020-12-21 11:52:55 -0800

    Day 21 fixed up

--- day21/21.py
+++ day21/21.py
@@ -1,48 +1,57 @@
 from collections import defaultdict
-foodList = open('input.txt','r').read().split('\n')
-candidates = defaultdict(set)
 
-foods = []
-allmap = defaultdict(set)
-ingmap = defaultdict(set)
+def parseFoods(foodList):
+    allergens = defaultdict(set)
+    ingredients = defaultdict(set)
 
-for i,food in enumerate(foodList):
-    ingredients,allergies = food.split(' (contains ')
-    ingredients = set(ingredients.split(' '))
-    allergies = allergies[:-1].split(', ')
+    for i,food in enumerate(foodList):
+        ings,allers = food.split(' (contains ')
+        ings = ings.split(' ')
+        allers = allers[:-1].split(', ')
+        for ing in ings:
+            ingredients[ing].add(i)
+        for aller in allers:
+            allergens[aller].add(i)
+    return ingredients, allergens
+
+def part1(ingredients, allergens):
+    count = 0
+    safe = set()
     for ing in ingredients:
-        ingmap[ing].add(i)
-    for _all in allergies:
-        allmap[_all].add(i)
+        flag = True
+        for aller in allergens:
+            if allergens[aller].issubset(ingredients[ing]):
+                flag = False
+                break
+        if flag:
+            count += len(ingredients[ing])
+            safe.add(ing)
+    for s in safe:
+        ingredients.pop(s)
+    return count, ingredients,allergens
+def part2(ingredients, allergens):
+    pairs = []
+    matrix = []
+    a = list(sorted(ingredients.keys()))
+    b = list(sorted(allergens.keys()))
+    for i in range(len(a)):
+        tmp2 = []
+        for j in range(len(b)):
+            tmp2.append(int(ingredients[a[i]].issuperset( allergens[b[j]])))
+        matrix.append(tmp2)
+    while sum(map(sum,matrix)) > 0:
+        for i,row in enumerate(matrix):
+            if sum(row) == 1:
+                col = row.index(1)
+                pairs.append((a[i],b[col]))
+                for j in range(len(matrix)):
+                    matrix[j][col] = 0
 
-c = 0
-safe = set()
-for ing in ingmap:
-    flag = True
-    for _all in allmap:
-        if allmap[_all].issubset(ingmap[ing]):
-            flag = False
-            break
-    if flag:
-        c += len(ingmap[ing])
-        safe.add(ing)
-for s in safe:
-    ingmap.pop(s)
-pairs = []
-matrix = []
-a = list(sorted(ingmap.keys()))
-b = list(sorted(allmap.keys()))
-for i in range(len(a)):
-    tmp2 = []
-    for j in range(len(b)):
-        tmp2.append(int(ingmap[a[i]].issuperset( allmap[b[j]])))
-    matrix.append(tmp2)
-while (sum([sum(x) for x in matrix]) > 0):
-    for i,row in enumerate(matrix):
-        if sum(row) == 1:
-            col = row.index(1)
-            pairs.append((a[i],b[col]))
-            for j in range(len(matrix)):
-                matrix[j][col] = 0
-print("".join([x[0] +',' for x in sorted(pairs,key = lambda x: x[1])])[:-1])
-print(c)
+    return "".join([x[0] +',' for x in sorted(pairs,key = lambda x: x[1])])[:-1]
+
+if __name__ == "__main__":
+    foodList = open('input.txt','r').read().split('\n')
+    ingredients, allergens = parseFoods(foodList)
+    p1c, ingredients, allergens = part1(ingredients,allergens)
+    p2 = part2(ingredients,allergens)
+    print("Part1: {} Part 2: {} ".format(p1c,p2))
